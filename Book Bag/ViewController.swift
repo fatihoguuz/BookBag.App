902b8f9d678bbefcbe9e150a395faced3aa975a5
Fatih Oguz <oguzfatih39@gmail.com>
2023-11-08 17:16:49 +0300

    Initial Commit

---
+++ Book Bag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Book Bag
+//
+//  Created by Fatih Oğuz on 8.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
